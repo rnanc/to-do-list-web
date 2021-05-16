4b9144caf637376b38bcac209fc29fd3fb346a49
Renan <renanfigcarneiro@gmail.com>
2021-05-16 17:04:09 -0300

    config: initial config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
+
+import GlobalStyles from './styles/global'
+
+const App = () => (
+    <BrowserRouter>
+      <GlobalStyles/>
+      <Routes/>
+    </BrowserRouter>
+  );
+
+
+export default App;
