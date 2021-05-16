e4ace098625793070d0335f319c80c26508f9537
Renan <renanfigcarneiro@gmail.com>
2021-05-16 19:01:09 -0300

    feat: calling api service

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
+import { ToastContainer, toast } from 'react-toastify';
 import { TextField } from '@material-ui/core'
 import { ChevronLeft } from '@material-ui/icons'
 import Button from '../../components/Button'
@@ -10,29 +11,54 @@ import {
   ContainerContent
  } from './styles'
 
+ import { createUser } from '../../services/userService'
 
-const SignIn = () => {
+
+const SignUp = () => {
   
-  const {register, handleSubmit} = useForm()
+  const {register, handleSubmit, errors, reset} = useForm()
+  const history = useHistory()
+  const timeOut = () => {setTimeout(()=>{history.push('/')}, 3000)}
 
-  const handleLogin = (data) => {
-    console.log(data);
+  const handleLogin = async (data) => {
+    console.log(data)
+    try {
+      await createUser(data)
+      toast.success('User created !', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        })
+        reset()
+        timeOut()
+    } catch (error) {
+      if(!errors){
+        toast.error(error)
+      }
+      toast.error(errors)
+      reset()
+    }
   };
 
   return (
   <Container>
     <div style={{display: 'flex', flex: 1}}></div>
     <ContainerContent>
-    <Link to="/signin"><ChevronLeft fontSize="large" /></Link>
-    <form onSubmit={handleSubmit(handleLogin)}>
-      <TextField label="Name" {...register('name')} />
-      <TextField label="E-mail" {...register('email')} />
-      <TextField type="password" label="Password" {...register('password')} />
-      <Button primary>Register</Button>
-    </form>
+      <Link to="/signin"><ChevronLeft fontSize="large" /></Link>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <ToastContainer />
+        <TextField label="Name" {...register('name', {required: true})} />
+        <TextField label="E-mail" type="email" {...register('email', {required: true})} />
+        <TextField type="password" label="Password" {...register('password', {required: true})} />
+        <Button primary>Register</Button>
+      </form>
     </ContainerContent>
   </Container>
 )};
 
 
-export default SignIn;
+export default SignUp;
